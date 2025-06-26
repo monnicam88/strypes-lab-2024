56d5152862f0f5e9f0418f4973fe30b5c578859f
monnica_88 <72393725+monnicam88@users.noreply.github.com>
2025-06-26 13:34:35 +0300

    Create exercise01-task01.cpp

---
+++ exercises/exercise01-task01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int a = 0, b = 0;
+
+    std::cout << "Enter two numbers: ";
+    std::cin >> a >> b;
+    std::cout << "The SUM is: " << (a + b) << std::endl;
+
+    return 0;
+}
