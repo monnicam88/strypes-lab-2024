e1e5378181eb84f2c1e5f1e4397de61300184dec
monnica_88 <72393725+monnicam88@users.noreply.github.com>
2025-06-26 14:18:45 +0300

    Create Constants.h

---
+++ project/include/Constants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace ProjectConstants {
+	static constexpr auto TRUTH_TABLE_CAPACITY = 100;
+	static constexpr auto CAPACITY_RESIZER = 2;
+	static constexpr auto MAX_ARRAY_SIZE = 100;
+	static constexpr auto MAX_STACK_SIZE = 100;
+	static constexpr auto MAX_CIRCUITS = 100;
+}
